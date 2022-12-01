e25cdfb3435d4e66f788d0f110779bac9825d8ac
FOTSO Claude <batfkc@gmail.com>
2022-12-01 11:24:08 +0100

    Token auth added

--- src/login/views.py
+++ src/login/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import login
-from django.contrib.auth.hashers import make_password
+from knox.models import AuthToken
 from rest_framework import generics, permissions
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from knox.views import LoginView as KnoxLoginView
@@ -17,11 +17,11 @@ class RegisterAPI(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        user.password = make_password(user.password)
+        user.set_password(user.password)
         user.save()
         return Response({
             "user": AuthSerializer(user, context=self.get_serializer_context()).data,
-            #  "token": AuthToken.objects.create(user)[1]
+            "token": AuthToken.objects.create(user)[1]
         })
 
 
--- src/training/views.py
+++ src/training/views.py
@@ -1,30 +1,37 @@
 from django.shortcuts import render
+from knox.auth import TokenAuthentication
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAdminUser, BasePermission, SAFE_METHODS
 
 from rest_framework.viewsets import ModelViewSet
 
 from training.models import Training, Category, Tag
 from training.serializers import TrainingSerializer, CategorySerializer, TagSerializer
-from rest_framework import status
-from rest_framework import generics, permissions
-from rest_framework.parsers import MultiPartParser, FormParser
-from rest_framework.response import Response
+
+
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
 
 
 class CategoryModelViewSet(ModelViewSet):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminUser | ReadOnly)
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
 class TagModelViewSet(ModelViewSet):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminUser | ReadOnly)
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
 
 class TrainingModelViewSet(ModelViewSet):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminUser | ReadOnly)
     queryset = Training.objects.all()
     serializer_class = TrainingSerializer
     # parser_classes = (MultiPartParser, FormParser)
     #  permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-
